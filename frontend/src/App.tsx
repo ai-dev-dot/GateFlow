56b31bd7aa0069a6cfee9322d17363e7bea53d03
ai-dev-dot <ai_dev_dot@163.com>
2026-06-04 22:42:14 +0800

    feat: 实现登录页面

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Navigate to="/chat" replace />} />
+      {/* Other routes will be added in Task 5 */}
     </Routes>
   )
 }
